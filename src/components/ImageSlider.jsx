bac890e7bece1e6ead0d4f5d6536e23291363638
coffmanjrp <coffmanjrp@gmail.com>
2021-06-28 23:13:22 -0700

    Display images

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import { SliderData } from '../utils/SliderData';
 
 export default function ImageSlider() {
-  return <div>Images</div>;
+  const [current, setCurrent] = useState(0);
+  const length = SliderData.length;
+
+  const handlePrev = () => {};
+
+  const handleNext = () => {};
+
+  return (
+    <section className="slider">
+      <div className="left-arrow" onClick={handlePrev}>
+        <FaArrowAltCircleLeft />
+      </div>
+      <div className="left-arrow" onClick={handleNext}>
+        <FaArrowAltCircleRight />
+      </div>
+      {SliderData.map((slide, index) => (
+        <img key={index} src={slide.image} alt={`slide-${index + 1}`} />
+      ))}
+    </section>
+  );
 }
